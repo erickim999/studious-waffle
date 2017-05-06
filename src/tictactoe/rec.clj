4a556aca2e2ae9f3bf2f0420746338eeb41517b8
Allan Jiang <origami789@gmail.com>
2017-05-05 22:06:53 -0700

    patch

--- src/tictactoe/rec.clj
+++ src/tictactoe/rec.clj
@@ -1,21 +1,4 @@
-(ns tictactoe.rec
-  (:refer-clojure :exclude [read])
-  (:require [clojure.string :as string]
-            [cognitect.transit :as t])
-  (:require-macros [tictactoe.rec :refer [slurp-dep]]))
+(ns tictactoe.rec)
 
-(def w (t/writer :json))
-(def r (t/reader :json))
-
-(def read (partial t/read r))
-(def write (partial t/write w))
-
-
-(defn hydrate-recording [file]
-  (let [chunks (-> file string/trim-newline string/split-lines)]
-    (mapcat t/read chunks)))
-
-
-(def rec (hydrate-recording (slurp-dep "resources/data/rJZfdFLcg.mtf")))
-
-
+(defmacro slurp-dep [path]
+  (slurp path))
--- src/tictactoe/rec.cljs
+++ src/tictactoe/rec.cljs
@@ -1,4 +1,21 @@
-(ns tictactoe.rec)
+(ns tictactoe.rec
+  (:refer-clojure :exclude [read])
+  (:require [clojure.string :as string]
+            [cognitect.transit :as t])
+  (:require-macros [tictactoe.rec :refer [slurp-dep]]))
 
-(defmacro slurp-dep [path]
-  (slurp path))
+(def w (t/writer :json))
+(def r (t/reader :json))
+
+(def read (partial t/read r))
+(def write (partial t/write w))
+
+
+(defn hydrate-recording [file]
+  (let [chunks (-> file string/trim-newline string/split-lines)]
+    (mapcat t/read chunks)))
+
+
+(def rec (hydrate-recording (slurp-dep "resources/data/rJZfdFLcg.mtf")))
+
+
